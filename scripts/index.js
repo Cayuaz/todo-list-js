972053999dd2c8124ddf1b7a97139a70fc92a340
Cayuã <cayuaronald7@gmail.com>
2025-09-15 19:59:17 -0400

    feat: implement actions of list buttons
    Implements the logic for the following action buttons: check/uncheck, edit item, delete item and cancel edit

--- scripts/index.js
+++ scripts/index.js
@@ -2,45 +2,39 @@ const form = document.getElementById("form")
 const addInput = document.getElementById("add-input")
 const list = document.getElementById("list")
 const addBtn = document.getElementById("add-btn")
+let listsItem = list.getElementsByTagName("li")
 
-//Propagação de evento
-list.addEventListener("click", (e) =>{
-    if(e.target.nodeName === "LI"){
-        console.log(e.target)
-    }
-})
 
 //Função de focus no input
-const inputFocus = () => addInput.focus()
+const inputFocus = () => addInput.focus();
 
 //Função que exibe a mensagem de erro
 function displayErrorMsg(error) {
 
-    const errorContainer = document.querySelector(".error-container")
-    const paragraphError = errorContainer.querySelector("p")
-    const closeBtn = errorContainer.querySelector("button")
+    const errorContainer = document.querySelector(".error-container");
+    const paragraphError = errorContainer.querySelector("p");
+    const closeBtn = errorContainer.querySelector("button");
 
-    errorContainer.classList.add("visible-display")
-    paragraphError.textContent = error
+    errorContainer.classList.add("visible-display");
+    paragraphError.textContent = error;
 
-    closeBtn.focus()
+    closeBtn.focus();
 
     function removeErrorMsg () {
-        errorContainer.classList.remove("visible-display")
-        inputFocus()
-        console.log("elemento removido")
-        closeBtn.removeEventListener("click", removeErrorMsg)
-        closeBtn.removeEventListener("keypress", removeErrorKey)
+        errorContainer.classList.remove("visible-display");
+        inputFocus();
+        closeBtn.removeEventListener("click", removeErrorMsg);
+        closeBtn.removeEventListener("keypress", removeErrorKey);
     }
 
     function removeErrorKey(e) {
         if(e.key === "Escape"){
-            removeErrorMsg()
+            removeErrorMsg();
         }
     }
 
-    closeBtn.addEventListener("click", removeErrorMsg)
-    closeBtn.addEventListener("keyup", removeErrorKey)
+    closeBtn.addEventListener("click", removeErrorMsg);
+    closeBtn.addEventListener("keyup", removeErrorKey);
          
 }
 
@@ -57,70 +51,102 @@ const arrTasks = [
 function createNewItemObj(obj){
 
     //Variável com a li que vai conter o novo item da lista
-    const listItem = document.createElement("li")
+    const listItem = document.createElement("li");
 
     //Container com o botão de check
+    const checkContainer = document.createElement("div");
 
-    //Container com o botão de check
-    const checkContainer = document.createElement("div")
-
-    checkContainer.classList.add("container-check-btn")
+    checkContainer.classList.add("container-check-btn");
 
     //Botão de check
-    const checkBtn =  document.createElement("button")
+    const checkBtn =  document.createElement("button");
+    //Atributo data check
+    checkBtn.setAttribute("data-action", "check");
 
-    checkBtn.classList.add("check-btn")
+    checkBtn.classList.add("check-btn");
 
     //Ícone de check
-    const checkIcon = document.createElement("i")
+    const checkIcon = document.createElement("i");
 
-    checkIcon.classList.add('fa-solid', 'fa-check', 'fa-sm')
+    checkIcon.classList.add('fa-solid', 'fa-check', 'fa-sm', `${obj.completed ? "visible" : "invisible"}`);
 
 
-    checkBtn.appendChild(checkIcon)
-    checkContainer.appendChild(checkBtn)
+    checkBtn.appendChild(checkIcon);
+    checkContainer.appendChild(checkBtn);
 
 
     //Parágrafo com o nome do item
-    const itemName = document.createElement("p")
+    const itemName = document.createElement("p");
 
-    itemName.textContent = obj.itemName
+    itemName.textContent = obj.itemName;
 
     //Adiciona o parágrafo ao check container
-    checkContainer.appendChild(itemName)
+    checkContainer.appendChild(itemName);
 
     //Container com os botões de editar e excluir 
 
     //Container com os botões de editar e deletar
-    const buttonsContainer = document.createElement("div")
+    const buttonsContainer = document.createElement("div");
 
-    buttonsContainer.classList.add("buttons-container")
+    buttonsContainer.classList.add("buttons-container");
 
     //Botão de editar
-    const editBtn =  document.createElement("button")
+    const editBtn =  document.createElement("button");
+    //Atributo data edit
+    editBtn.setAttribute("data-action", "edit");
+    
+    //Ícone de editar
+    const editIcon = document.createElement("i") ;
 
-    //Ícone de ediatr
-    const editIcon = document.createElement("i")
-
-    editIcon.classList.add('fa-solid', 'fa-pen-to-square')
-    editBtn.appendChild(editIcon)
+    editIcon.classList.add('fa-solid', 'fa-pen-to-square');
+    editBtn.appendChild(editIcon);
 
     //Botão de excluir
-    const deleteBtn =  document.createElement("button")
+    const deleteBtn =  document.createElement("button");
+    //Atributo data delete
+    deleteBtn.setAttribute("data-action", "delete");
 
     //Ícone de deletar
-    const deleteIcon = document.createElement("i")
+    const deleteIcon = document.createElement("i");
 
-    deleteIcon.classList.add('fa-solid', 'fa-trash')
-    deleteBtn.appendChild(deleteIcon)
+    deleteIcon.classList.add('fa-solid', 'fa-trash');
+    deleteBtn.appendChild(deleteIcon);
 
     //Adiciona os botões de editar e excluir ao container
-    buttonsContainer.appendChild(editBtn)
-    buttonsContainer.appendChild(deleteBtn)
+    buttonsContainer.appendChild(editBtn);
+    buttonsContainer.appendChild(deleteBtn);
+
+
+    //Container de edição
+    const editContainer = document.createElement("div");
+    editContainer.classList.add("edit-container");
+
+    const editInput = document.createElement("input");
+    editInput.setAttribute("type", "text");
+    editInput.classList.add("edit-input");
+    editInput.value = obj.itemName
+
+    const btnConfirmEdit = document.createElement("button");
+    btnConfirmEdit.classList.add("edit-btn");
+    //Atributo data confirmEdit
+    btnConfirmEdit.setAttribute("data-action", "confirmEdit");
+    btnConfirmEdit.textContent = "Edit";
+
+    const btnCancel = document.createElement("button");
+    btnCancel.classList.add("cancel-btn");
+    //Atributo data cancel
+    btnCancel.setAttribute("data-action", "cancel");
+    btnCancel.textContent = "Cancel"
+
+    editContainer.appendChild(editInput);
+    editContainer.appendChild(btnConfirmEdit);
+    editContainer.appendChild(btnCancel);
+
 
     //Adiciona o container de check, o parágrafo com o nome do item e o container com os botões à li
-    listItem.appendChild(checkContainer)
-    listItem.appendChild(buttonsContainer)
+    listItem.appendChild(checkContainer);
+    listItem.appendChild(buttonsContainer);
+    listItem.appendChild(editContainer);
 
     //Adiciona a nova li à lista
     // list.appendChild(listItem)
@@ -135,7 +161,7 @@ function displayNewItem(create) {
     //Limpa antes para não exibir tarefas repetidas, já que percorre todos os objetos do array de tarefas
     list.innerHTML = "";
     arrTasks.forEach((task) => {
-        list.appendChild(create(task))
+        list.appendChild(create(task));
     })
 }
 
@@ -151,9 +177,9 @@ function addTask(taskName) {
 //Função que faz a verifica se o input não está vazio
 const checkInput = input => {
     if(input.value){
-        return true
+        return true;
     } else {
-        throw new Error("O campo do formulário não pode estar vazio")
+        throw new Error("O campo do formulário não pode estar vazio");
     }
 }
 
@@ -162,22 +188,106 @@ function process(check, add, displayItem, create, displayError) {
 
     try {
         check(addInput);
-        add(addInput.value)
-        displayItem(create)
+        add(addInput.value);
+        displayItem(create);
         addInput.value = "";
-        inputFocus()
+        inputFocus();
     } catch (error) {
-        displayError(error.message)
+        displayError(error.message);
     }
 }
 
 //Evento de submit, que chama a função process e passa as outras funções como argumento
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    process(checkInput, addTask, displayNewItem, createNewItemObj, displayErrorMsg)
+    e.preventDefault();
+    process(checkInput, addTask, displayNewItem, createNewItemObj, displayErrorMsg);
 
 })
 
-displayNewItem(createNewItemObj)
+function getIndex(arr, element){
+   arr = Array.from(arr)
+   return arr.indexOf(element)
+}
+
+function clickListItems(e){
+
+    // console.log(e.target)
+    const clickedElement = e.target.closest("button");
+    if(!clickedElement) return
+    const dataAction = clickedElement.getAttribute("data-action");
+
+    const listItem = clickedElement.closest("li")
+    
+    const editContainer = listItem.querySelector(".edit-container");
+    console.log(editContainer);
+
+    const inputEdit = editContainer.querySelector("input")
+
+    const index = getIndex(listsItem, listItem)
+    console.log(index);
+
+        const datas = {
+
+            check: function(){
+                console.log("check");
+                const checkIconClick = listItem.querySelector(".fa-check")
+                arrTasks[index].completed = !arrTasks[index].completed
+
+                if(arrTasks[index].completed){
+                    checkIconClick.classList.add("visible")
+                } else {
+                    checkIconClick.classList.remove("visible")
+                }
+
+                displayNewItem(createNewItemObj)
+            },
+
+            edit: function(){
+
+                console.log("edit");
+
+                [...list.querySelectorAll(".edit-container")].forEach(container => {
+                    container.classList.remove("visible-display")
+                })
+
+                editContainer.classList.add("visible-display")
+            },
+
+            delete: function(){
+                console.log("delete")
+                arrTasks.splice(index, 1)
+                // listItem.remove()
+                displayNewItem(createNewItemObj)
+            },
+
+            confirmEdit: function(){
+
+                console.log("confirmEdit")
+                console.log(inputEdit)
+        
+                try {
+                    checkInput(inputEdit)
+                } catch (error) {
+                    displayErrorMsg(error.message)
+                    return
+                }
+                arrTasks[index].itemName = inputEdit.value
+                displayNewItem(createNewItemObj)
+            },
+
+            cancel: function(){
+                console.log("cancel")
+                editContainer.classList.remove("visible-display")
+                inputEdit.value = arrTasks[index].itemName
+            }
+
+        }
+
+        datas[dataAction]();
+}
+
+list.addEventListener("click", clickListItems);
+
+displayNewItem(createNewItemObj);
 
                         
